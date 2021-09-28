28b7c25ddf95212113fe310f2fa4c14fbed8f673
Polina <polinageiko10@gmail.com>
2021-09-28 20:38:56 -0300

    feat: implement a transform array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,39 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  var r = 0
+    if (Array.isArray(arr)) {
+    for (var i = 0; i < arr.length; i)
+    {
+      if (arr[i] === '--discard-next') { 
+        if (i != arr.length) { 
+          arr.splice(i, 2) 
+          r = i
+        }
+        else { arr.splice(i, 1) }
+      }
+      else if (arr[i] === '--discard-prev') { 
+        if (i != 0) { 
+          arr.splice(i - 1, 2) 
+        }
+        else { arr.splice(i, 1) }
+        
+      }
+      else if (arr[i] === '--double-next') { 
+        if (i != arr.length) { arr.splice(i, 1, arr[i+1]) }
+        else { arr.splice(i, 1) }
+      }
+      else if (arr[i] === '--double-prev') {
+        if (i != 0 && i != r) { arr.splice(i, 1, arr[i-1]) }
+        else { arr.splice(i, 1) }
+      }
+      else {
+        i++
+      }
+    }
+    return arr
+  }
+
+  throw new Error ("'arr' parameter must be an instance of the Array!")
 }
