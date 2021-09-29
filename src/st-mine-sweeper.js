67ed0e48c2e28e58d37113b68eea450569b7119b
Polina <polinageiko10@gmail.com>
2021-09-29 02:02:37 -0300

    feat: implement a st mine sweeper solution

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -23,7 +23,23 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper (matrix) {
+  var matrix2 = [...Array(matrix.length)].map(e => Array(matrix[0].length))
+
+  var rowLimit = matrix.length-1;
+  var columnLimit = matrix[0].length-1;
+  var num = 0
+  
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      num = 0
+      for(var x = Math.max(0, i-1); x <= Math.min(i+1, rowLimit); x++) {
+        for(var y = Math.max(0, j-1); y <= Math.min(j+1, columnLimit); y++) {
+          if((x !== i || y !== j) && matrix[x][y] === true) { num++ }
+        }
+      }
+      matrix2[i][j] = num
+    }
+  }
+  return matrix2
 }
